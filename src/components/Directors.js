0770eb34ce708d777a9bce58a523c80146b89633
kevinneyer <kevinneyer@gmail.com>
2020-07-22 16:23:58 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -5,6 +5,17 @@ const Directors = () => {
   return (
     <div>
       {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map( director =>
+        <div>
+          <h3>Name: {director.name}</h3>
+          <p>Movies:
+          <ul>
+          {director.movies.map( movie => <li>{movie}</li>)}
+          </ul>
+          </p>
+        </div>
+      )}
     </div>
   );
 }
